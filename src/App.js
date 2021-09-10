f87359f342cdb2d129228298e05c0176e99b38e4
Hưng Nguyễn <hungamtp@gmail.com>
2021-09-11 06:18:20 +0700

    login

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React  , {useState } from 'react';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 import "./app.css";
+import { Button, TextField } from '@material-ui/core';
 
 export default function App() {
 
   const [x , setX] = useState(12);
   const [y , setY] = useState(12);
+  const [authenticated , setAuthenticated] = useState(false);
+  const [pass , setPass] = useState("");
+  const login = () => {
+  if(pass==="dit con me may luon"){
+    setAuthenticated(true)
+  }
+  }
 
   return (
     <div className="App" onMouseMove={(e) => {
@@ -14,8 +22,17 @@ export default function App() {
       setY(e.clientY);
     }}>
       <div id ="back" >html{x}-{y}</div>
-      <Form />
-      <TodoList />
+      {authenticated ? <><Form className="front"/>
+      <TodoList className="front"/></> :<div className="login-form">
+      <TextField
+          id="filled-size-small"
+          size="small"
+          type="password"
+          onChange={(e) => setPass(e.target.value)} value={pass}
+        />
+      <button className="loginButton" onClick={login}>==========================> </button>
+      </div>}
+      
     </div>
   );
 }
