f463d9b4bee98ed38ea4f2227fb11e08350cd387
Hưng Nguyễn <hungamtp@gmail.com>
2021-09-11 06:48:13 +0700

    Updates

---
+++ precache-manifest.a1a3fe1546f5c526e63c1c9cb69683ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e346d63539b7f947838058914ff23b",
+    "url": "/7mondays/index.html"
+  },
+  {
+    "revision": "5490ab6a7e8f0ef26fd3",
+    "url": "/7mondays/static/css/main.4f2db0b4.chunk.css"
+  },
+  {
+    "revision": "adeb3b66f395ecabac81",
+    "url": "/7mondays/static/js/2.06bc0f07.chunk.js"
+  },
+  {
+    "revision": "fbfb5e42a41e7004782949e93fdf296a",
+    "url": "/7mondays/static/js/2.06bc0f07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5490ab6a7e8f0ef26fd3",
+    "url": "/7mondays/static/js/main.72966dac.chunk.js"
+  },
+  {
+    "revision": "f74284fba9c9fe5fe22b",
+    "url": "/7mondays/static/js/runtime-main.cdb879f7.js"
+  }
+]);
