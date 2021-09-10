b5c87b2d2efe9f76e890d6d7423b138fc45a704b
Hưng Nguyễn <hungamtp@gmail.com>
2021-09-11 03:07:45 +0700

    s

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
-import UploadImage from './components/UploadImage';
 
 export default function App() {
   return (
@@ -10,7 +9,6 @@ export default function App() {
       <h1>Todo</h1>
       <Form />
       <TodoList />
-      <UploadImage />
     </div>
   );
 }
--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD913zoD5TQwGsd1kHWPdvzo6-7DMFBj8U',
-  authDomain: 'tododaily-5dc59.firebaseapp.com',
-  databaseURL: 'https://tododaily-5dc59.firebaseio.com',
-  projectId: 'tododaily-5dc59',
-  storageBucket: 'tododaily-5dc59.appspot.com',
-  messagingSenderId: '272694852596',
-  appId: '1:272694852596:web:5ddb762dd9e30e4cb0d7a1',
-  measurementId: 'G-BSXVN1XVL7',
+  apiKey: "AIzaSyDeikuzWNX8E3MeNivbzdfjcbZ6AK8h14k",
+  authDomain: "to-fukcin-do.firebaseapp.com",
+  projectId: "to-fukcin-do",
+  storageBucket: "to-fukcin-do.appspot.com",
+  messagingSenderId: "719198772522",
+  appId: "1:719198772522:web:683d846e93494b4bdd9253",
+  measurementId: "G-4GYHPDZBQS"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
