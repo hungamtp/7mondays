da868b6b0874e08d641e133be4bcc1ea8ba43e39
Hưng Nguyễn <hungamtp@gmail.com>
2021-09-11 06:37:06 +0700

    log out

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React  , {useState } from 'react';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 import "./app.css";
-import { Button, TextField } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
 
 export default function App() {
 
   const [x , setX] = useState(12);
   const [y , setY] = useState(12);
-  const [authenticated , setAuthenticated] = useState(false);
+  const [authenticated , setAuthenticated] = useState(localStorage.getItem("auth"));
   const [pass , setPass] = useState("");
   const login = () => {
   if(pass==="dit con me may luon"){
     setAuthenticated(true)
+    localStorage.setItem("auth" , true);
   }
+  }
+  const logout = () =>{
+    localStorage.removeItem("auth");
+    setAuthenticated(false)
   }
 
   return (
@@ -21,18 +26,20 @@ export default function App() {
       setX(e.clientX);
       setY(e.clientY);
     }}>
-      <div id ="back" >html{x}-{y}</div>
+      
       {authenticated ? <><Form className="front"/>
-      <TodoList className="front"/></> :<div className="login-form">
+      <TodoList className="front"/>
+      <button id ="logout" onClick={logout}>&#60;==============================</button>
+      </> :<div className="login-form">
       <TextField
           id="filled-size-small"
           size="small"
           type="password"
           onChange={(e) => setPass(e.target.value)} value={pass}
         />
-      <button className="loginButton" onClick={login}>==========================> </button>
+      <button className="loginButton" onClick={login}>==========================&#62; </button>
       </div>}
-      
+      <div id ="back" >html:{x}px-{y}px</div>
     </div>
   );
 }
